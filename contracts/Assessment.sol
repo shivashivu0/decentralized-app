996ab8e8b23d0fbd388da4c7683b4513f65d2402
shivashivu0 <148582234+shivashivu0@users.noreply.github.com>
2024-02-04 18:14:35 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,22 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    string public ownerName;
+    uint256 public age;
+    string public gender;
+    uint256 public loans;
+    uint256 public minDeposit;
+    uint256 public minWithdrawal;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event LimitSet(uint256 minDeposit, uint256 minWithdrawal);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+        ownerName = "Shivkumar";
+        age = 20;
+        gender = "Male";
+        loans = 500;
+        minDeposit = 1 ether; // Default minimum deposit amount
+        minWithdrawal = 1 ether; // Default minimum withdrawal amount
     }
 
     function getBalance() public view returns(uint256){
         return balance;
+    }
+
+    function getOwnerInfo() public view returns(string memory, uint256, string memory, uint256) {
+        return (ownerName, age, gender, loans);
+    }
+
+    function setLimit(uint256 _minDeposit, uint256 _minWithdrawal) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        minDeposit = _minDeposit;
+        minWithdrawal = _minWithdrawal;
+        emit LimitSet(_minDeposit, _minWithdrawal);
     }
 
     function deposit(uint256 _amount) public payable {
@@ -24,6 +46,7 @@ contract Assessment {
 
         // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
+        require(_amount >= minDeposit, "Deposit amount is less than minimum limit");
 
         // perform transaction
         balance += _amount;
@@ -41,6 +64,8 @@ contract Assessment {
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
+        require(_withdrawAmount >= minWithdrawal, "Withdrawal amount is less than minimum limit");
+
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
